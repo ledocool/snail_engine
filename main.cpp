4292c1bf987ddd4e2f3efbe1f7c24cafb5ca2d9b
LedoCool <ledocool-89@yandex.ru>
2018-07-12 22:46:41 +0700

    Hello world built 'n' tested

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    //std::cout << "Hello world!" << std::endl;
+    std::cout << "Hello world!" << std::endl;
     Engine e = Engine();
     e.simpleFunction();
     return 0;
